ef6b786d1bd19f70321655c81599081e0d854e95
jah71 <jah71aber@gmail.com>
2020-07-26 18:13:37 +0100

    two basic tests mocked + implemented

--- server.js
+++ server.js
@@ -11,27 +11,21 @@ const converter = new showdown.Converter();
 
 const { JSDOM } = require('jsdom');
 
-app.use(express.static(__dirname + 'public'));
-
 /**
 * This is loaded syncrounously, as it's only necessary to load once
 */
 const templateFile = fs.readFileSync('./template.html', (err, template) => {
-   if (err) console.error(err);
-   return template.file.toString();
+    if (err) console.error(err);
+    return template.file.toString();
 });
 
-app.get('*', (req, res, next) => {
+app.get('*', (req, res) => {
     // TODO sanitise the URL, check if a file exists at that url, otherwise redirect
     const contentLocation = content + req.url + file;
 
     fs.readFile(contentLocation, (err, data) => {
-        processContent(err, data, res, next);
+        processContent(err, data, res);
     });
-}, (req, res) => {
-    res.type('html');
-    res.status(200);
-    res.send(appendContentToTemplate(res.content));
 });
 
 /**
@@ -39,27 +33,33 @@ app.get('*', (req, res, next) => {
  * are 500 server errors, otherwise we convert the markdown to html and move
  * on to the next piece of middleware
  */
-function processContent(err, data, res, next) {
-    if (err && err.errno === -4058) {
+function processContent(err, data, res) {
+    if (err && err.code === 'ENOENT') {
         res.status(404).send('404');
     } else if (err) {
         console.error(err);
         res.status(500).send('500');
     } else {
-        res.content = converter.makeHtml(data.toString());
-        next();
+        res.status(200).send(appendContentToTemplate(data.toString()));
     }
 }
 
 /**
  * Adds the passed in content to the placeholder in the body of the template.html file
  */
-function appendContentToTemplate(content) {
-    let dom = new JSDOM(templateFile);
-    dom.window.document.body.innerHTML = new JSDOM(content).serialize();
+function appendContentToTemplate(data) {
+    const dom = new JSDOM(templateFile);
+    const content = new JSDOM(converter.makeHtml(data));
+
+    dom.window.document.body.innerHTML = content.serialize();
     return converter.makeHtml(dom.serialize());
 }
 
 app.listen(port, () => {
     console.log(`Server listening on the port::${port}`);
 });
+
+module.exports = {
+    processContent: processContent,
+    appendContentToTemplate: appendContentToTemplate
+}
--- spec/RoutingSpec.js
+++ spec/RoutingSpec.js
@@ -1,11 +1,41 @@
+let server = require('../server');
+const request = require('supertest');
+
 describe('Routing', function() {
 
+    const data = '<p>test</p>';
+
+    const mockResponse = {
+        status: function(httpStatus) {
+            return this;
+        },
+        send: function() {
+            return this;
+        }
+    }
+
+    beforeAll(function() {
+        spyOn(mockResponse, 'status').and.callThrough();
+        spyOn(mockResponse, 'send');
+    });
+
     it('should return a 404 error if the resource doesn`t exist', function() {
-        expect(true).toBe(true);
+        server.processContent({code: 'ENOENT'}, undefined, mockResponse);
+
+        expect(mockResponse.status).toHaveBeenCalledWith(404);
+        expect(mockResponse.send).toHaveBeenCalled();
     });
 
     it('should return a 200 when the resource does exist', function() {
-        expect(true.toBe(true));
-    })
+        server.processContent(undefined, data, mockResponse);
+        expect(mockResponse.status).toHaveBeenCalledWith(200);
+        expect(mockResponse.send).toHaveBeenCalled();
+    });
+
+    it('should contain the correct content', function() {
+        spyOn(server, 'appendContentToTemplate');
+        server.processContent(undefined, data, mockResponse);
+        expect(server.appendContentToTemplate).toHaveBeenCalled();
+    });
 
 });
